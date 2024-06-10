f84804447d59b188fca8dbe8e8ea240ee465457d
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2024-06-10 16:55:25 +0400

    fix(Table): remove unneccessary comments

--- src/generalComponents/table/Table.jsx
+++ src/generalComponents/table/Table.jsx
@@ -2,77 +2,6 @@ import React, { useState } from 'react';
 import { Space, Table } from 'antd';
 import { BsFillTrashFill, BsFillPencilFill } from "react-icons/bs";
 
-// const columns = [
-//   {
-//     title: 'Full Name',
-//     width: 100,
-//     dataIndex: 'name',
-//     key: 'name',
-//     fixed: 'left',
-//   },
-//   {
-//     title: 'Age',
-//     width: 100,
-//     dataIndex: 'age',
-//     key: 'age',
-//     fixed: 'left',
-//   },
-//   {
-//     title: 'Column 1',
-//     dataIndex: 'address',
-//     key: '1',
-//     width: 150,
-//   },
-//   {
-//     title: 'Column 2',
-//     dataIndex: 'address',
-//     key: '2',
-//     width: 150,
-//   },
-//   {
-//     title: 'Column 3',
-//     dataIndex: 'address',
-//     key: '3',
-//     width: 150,
-//   },
-//   {
-//     title: 'Column 4',
-//     dataIndex: 'address',
-//     key: '4',
-//     width: 150,
-//   },
-//   {
-//     title: 'Column 5',
-//     dataIndex: 'address',
-//     key: '5',
-//     width: 150,
-//   },
-//   {
-//     title: 'Column 6',
-//     dataIndex: 'address',
-//     key: '6',
-//     width: 150,
-//   },
-//   {
-//     title: 'Column 7',
-//     dataIndex: 'address',
-//     key: '7',
-//     width: 150,
-//   },
-//   {
-//     title: 'Column 8',
-//     dataIndex: 'address',
-//     key: '8',
-//   },
-//   {
-//     title: 'Action',
-//     key: 'operation',
-//     fixed: 'right',
-//     width: 100,
-//     render: () => <a>action</a>,
-//   },
-// ];
-
 const TableComponent = ({ 
     whichTable, 
     datas,
@@ -80,8 +9,6 @@ const TableComponent = ({
     onClickEditButton, 
     onClickDeleteButton 
 }) => {
-    // const [fixedTop, setFixedTop] = useState(false);
-
     const terminalsColumns = [
         {
             title: 'ID',
@@ -301,30 +228,6 @@ const TableComponent = ({
             columns={columns}            
             dataSource={whichTable === "users" ? data : datas}
             pagination={false}
-            //   scroll={{
-            //     x: 1500,
-            //   }}
-            //   summary={() => (
-            //     <Table.Summary fixed={fixedTop ? 'top' : 'bottom'}>
-            //       <Table.Summary.Row>
-            //         <Table.Summary.Cell index={0} colSpan={2}>
-            //           <Switch
-            //             checkedChildren="Fixed Top"
-            //             unCheckedChildren="Fixed Top"
-            //             checked={fixedTop}
-            //             onChange={() => {
-            //               setFixedTop(!fixedTop);
-            //             }}
-            //           />
-            //         </Table.Summary.Cell>
-            //         <Table.Summary.Cell index={2} colSpan={8}>
-            //           Scroll Context
-            //         </Table.Summary.Cell>
-            //         <Table.Summary.Cell index={10}>Fix Right</Table.Summary.Cell>
-            //       </Table.Summary.Row>
-            //     </Table.Summary>
-            //   )}
-            // antd site header height
             sticky={{
                 offsetHeader: 64,
             }}
