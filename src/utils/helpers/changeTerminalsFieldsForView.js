b13a2c7c54156f46c90193a6a0366fdc7bd18df4
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2024-08-07 12:30:05 +0400

    fix(changeTerminalsFieldsForView): change date format with dayjs lib and change time depends on local time

--- src/utils/helpers/changeTerminalsFieldsForView.js
+++ src/utils/helpers/changeTerminalsFieldsForView.js
@@ -1,9 +1,13 @@
+import * as dayjs from "dayjs";
+
 export const changeTerminalsFieldsForView = (items, currentPage, pageSize) => {
+    const addingHours =  - new Date().getTimezoneOffset() / 60;
+
     items.map((item) => {
         item.number = items.indexOf(item) + 1 + (currentPage - 1) * pageSize;
-        item.createdAt = item.createdAt.slice(0, 10) + " " + item.createdAt.slice(11, 19);
+        item.createdAt = dayjs(item.createdAt).add(addingHours, "hour").format("DD-MM-YYYY HH:mm:ss");
         if (item.inactiveDate) {
-            item.inactiveDate = item.inactiveDate.slice(0, 10) + " " + item.inactiveDate.slice(11, 19);
+            item.inactiveDate = dayjs(item.createdAt).add(addingHours, "hour").format("DD-MM-YYYY HH:mm:ss");
         }
     });
 
